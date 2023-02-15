b22ed733396cf7b7fb6d8873d062a08988cf2a06
tajkir-alam <tajkir.alam@gmail.com>
2023-02-15 12:39:19 +0600

    total some facing problem

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 priceAdd('action-cart', 'action-cam-price', 'action-cam-cost');
+// increaseAmount('action-cart', 'action-cam-price', 'action-cam-cost');
+
+priceAdd('camera-cart', 'dslr-cam-price', 'dslr-cam-cost');
+
+priceAdd('drone-cart', 'drone-price', 'drone-cost');
+
+priceAdd('mic-cart', 'mic-price', 'mic-cost');
+
+
+total();
--- js/utilities.js
+++ js/utilities.js
@@ -5,3 +5,29 @@ function priceAdd(cartId, priceIs, priceAdded){
         price.innerText = amount;
     })
 }
+
+// function increaseAmount(cartId, priceIs, priceAdded){
+//     document.getElementById(cartId).addEventListener('mouseenter', function(){
+//         const valueIncrease = document.getElementById(priceIs).innerText;
+//         const value = parseFloat(valueIncrease); 
+//         const ok = value *2;
+//         const price = document.getElementById(priceAdded);
+//         price.innerText = ok;
+//     })
+// }
+
+
+
+// Sum of all Products
+function total(){
+    const actionCamera = document.getElementById('action-cam-cost');
+    const DSLRCamera = document.getElementById('dslr-cam-cost');
+    const mediumDrone = document.getElementById('drone-cost');
+    const coverMic = document.getElementById('mic-cost');
+
+    const totalAmount = parseFloat(actionCamera) + parseFloat(DSLRCamera) + parseFloat(mediumDrone) + parseFloat(coverMic);
+    
+    const totalinText = document.getElementById('total-bill');
+    const totalBill = parseFloat(totalinText);
+    totalBill.innerText = totalAmount;
+}
